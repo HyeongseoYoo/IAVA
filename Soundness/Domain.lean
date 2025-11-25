1be277774c41eb354f9f9f691ad028828d3447b7
HyeongseoYoo <hyeongseo0918@gmail.com>
2025-11-25 19:11:12 +0900

    add value domain

--- Soundness/Domain.lean
+++ Soundness/Domain.lean
@@ -4,14 +4,16 @@ import Mathlib.Tactic.Linarith
 
 open Set
 
+/-- Abstract Z -/
 inductive Itv
 | bot
 | mk (lo hi : ℤ) (h : lo ≤ hi)
+deriving Repr
 
 def Itv.interval (lo hi : ℤ) : Itv :=
   if h : lo ≤ hi then Itv.mk lo hi h else Itv.bot
 
-def γ : Itv → Set ℤ
+def γItv : Itv → Set ℤ
 | Itv.bot        => ∅
 | (Itv.mk lo hi _) => {z | lo ≤ z ∧ z ≤ hi}
 
@@ -26,3 +28,56 @@ def add_abs (I J : Itv) : Itv :=
       Itv.mk (lo₁ + lo₂) (hi₁ + hi₂) (by linarith)
 
 notation I " ⊕ " J => add_abs I J
+
+/-- Concrete locations -/
+abbrev Base := ℤ
+abbrev Offset := ℤ
+
+structure Loc where
+  base : Base
+  offset : Offset
+deriving Repr, DecidableEq
+
+/-- Abstract locations -/
+abbrev Label := Nat
+
+structure AbsLoc where
+  allocSite : Label
+  offset : Itv
+deriving Repr
+
+def γLoc (env : Label → Set Base) (absℓ : AbsLoc) : Set Loc :=
+  { ℓ : Loc | ℓ.base ∈ env absℓ.allocSite ∧ ℓ.offset ∈ γItv absℓ.offset }
+
+/-- Concrete Unit -/
+notation "⋅" => ()
+
+/-- Abstract Unit -/
+inductive AbsUnit where
+| bot
+| unit
+deriving Repr, DecidableEq
+
+def γUnit : AbsUnit → Set Unit
+| AbsUnit.bot  => ∅
+| AbsUnit.unit => {⋅}
+
+/-- Concrete Value -/
+inductive Value where
+| int (z: ℤ)
+| loc (ℓ : Loc)
+| unit (u : Unit)
+deriving Repr, DecidableEq
+
+/-- Abstract Value -/
+structure AbsValue where
+  absint : Itv
+  absloc : AbsLoc
+  absunit : AbsUnit
+deriving Repr
+
+def γValue (env : Label → Set Base) (absv : AbsValue) : Set Value :=
+  let intSet := {Value.int z | z ∈ γItv absv.absint}
+  let locSet := {Value.loc ℓ | ℓ ∈ γLoc env absv.absloc}
+  let unitSet := {Value.unit u | u ∈ γUnit absv.absunit}
+  intSet ∪ locSet ∪ unitSet
--- Soundness/GammaSound.lean
+++ Soundness/GammaSound.lean
@@ -6,25 +6,25 @@ import Soundness.Domain
 open Set
 
 lemma add_abs_sound_γ (I J : Itv) :
-  add (γ I) (γ J) ⊆ γ (I ⊕ J) := by
+  add (γItv I) (γItv J) ⊆ γItv (I ⊕ J) := by
   intro z hz
   unfold add at hz
   cases I with
   | bot =>
       cases J with
       | bot =>
-          simp [γ] at hz
+          simp [γItv] at hz
       | mk lo₂ hi₂ h₂ =>
-          simp [γ] at hz
+          simp [γItv] at hz
   | mk lo₁ hi₁ h₁ =>
       cases J with
       | bot =>
-          simp [γ] at hz
+          simp [γItv] at hz
       | mk lo₂ hi₂ h₂ =>
           rcases hz with ⟨x, hx, y, hy, rfl⟩
           rcases hx with ⟨hx_lo, hx_hi⟩
           rcases hy with ⟨hy_lo, hy_hi⟩
-          simp [γ, add_abs]
+          simp [γItv, add_abs]
           constructor
           · linarith
           · linarith
