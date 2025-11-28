8e32ea4c3a9aab4aa08252aa7b54165fbbff8296
HyeongseoYoo <hyeongseo0918@gmail.com>
2025-11-28 15:23:25 +0900

    add 'add operator' soundness

--- Soundness/Domain.lean
+++ Soundness/Domain.lean
@@ -17,18 +17,6 @@ def γItv : Itv → Set ℤ
 | Itv.bot        => ∅
 | (Itv.mk lo hi _) => {z | lo ≤ z ∧ z ≤ hi}
 
-def add (S T : Set ℤ) : Set ℤ :=
-  {z | ∃ x ∈ S, ∃ y ∈ T, z = x + y}
-
-def add_abs (I J : Itv) : Itv :=
-  match I, J with
-  | Itv.bot, _ => Itv.bot
-  | _, Itv.bot => Itv.bot
-  | Itv.mk lo₁ hi₁ h₁, Itv.mk lo₂ hi₂ h₂ =>
-      Itv.mk (lo₁ + lo₂) (hi₁ + hi₂) (by linarith)
-
-notation I " ⊕ " J => add_abs I J
-
 /-- Concrete locations -/
 abbrev Base := ℤ
 abbrev Offset := ℤ
@@ -39,15 +27,17 @@ structure Loc where
 deriving Repr, DecidableEq
 
 /-- Abstract locations -/
-abbrev Label := Nat
+abbrev ℙ := Nat
 
-structure AbsLoc where
-  allocSite : Label
-  offset : Itv
+inductive AbsLoc where
+| bot
+| mk (allocSite : ℙ) (offset : Itv)
 deriving Repr
 
-def γLoc (env : Label → Set Base) (absℓ : AbsLoc) : Set Loc :=
-  { ℓ : Loc | ℓ.base ∈ env absℓ.allocSite ∧ ℓ.offset ∈ γItv absℓ.offset }
+def γLoc (env : ℙ → Set Base) (absℓ : AbsLoc) : Set Loc :=
+match absℓ with
+| AbsLoc.bot => ∅
+| AbsLoc.mk allocSite offset => { ℓ : Loc | ℓ.base ∈ env allocSite ∧ ℓ.offset ∈ γItv offset }
 
 /-- Concrete Unit -/
 notation "⋅" => ()
@@ -76,8 +66,51 @@ structure AbsValue where
   absunit : AbsUnit
 deriving Repr
 
-def γValue (env : Label → Set Base) (absv : AbsValue) : Set Value :=
+def γValue (penv : ℙ → Set Base) (absv : AbsValue) : Set Value :=
   let intSet := {Value.int z | z ∈ γItv absv.absint}
-  let locSet := {Value.loc ℓ | ℓ ∈ γLoc env absv.absloc}
+  let locSet := {Value.loc ℓ | ℓ ∈ γLoc penv absv.absloc}
   let unitSet := {Value.unit u | u ∈ γUnit absv.absunit}
   intSet ∪ locSet ∪ unitSet
+
+/-- Semantic operators -/
+
+def add (S T : Set ℤ) : Set ℤ :=
+  {z | ∃ x ∈ S, ∃ y ∈ T, z = x + y}
+
+def addVal (S T : Set Value) : Set Value :=
+  { v | ∃ z1 z2,
+      v = Value.int (z1 + z2)
+      ∧ Value.int z1 ∈ S
+      ∧ Value.int z2 ∈ T }
+
+def addAbs_itv (I J : Itv) : Itv :=
+  match I, J with
+  | Itv.bot, _ => Itv.bot
+  | _, Itv.bot => Itv.bot
+  | Itv.mk lo₁ hi₁ h₁, Itv.mk lo₂ hi₂ h₂ =>
+      Itv.mk (lo₁ + lo₂) (hi₁ + hi₂) (by linarith)
+
+notation I " ⊕ " J => addAbs_itv I J
+
+def addAbs (v1 v2 : AbsValue) : AbsValue :=
+  let absint :=
+    match v1.absint, v2.absint with
+    | I, J => I ⊕ J
+  AbsValue.mk absint AbsLoc.bot AbsUnit.bot
+
+-- def equal : Value → Value → Prop
+-- | Value.int z1, Value.int z2 => z1 = z2
+-- | Value.loc ℓ1, Value.loc ℓ2 => ℓ1 = ℓ2
+-- | Value.unit _, Value.unit _ => True
+-- | _, _ => False
+
+-- def eqAbs (v1 v2: AbsValue) : Prop :=
+--   let int_eq := match v1.absint, v2.absint with
+--                  | Itv.bot, _ => False
+--                  | _, Itv.bot => False
+--                  | Itv.mk lo₁ hi₁ h₁, Itv.mk lo₂ hi₂ h₂ =>
+--                      lo₁ = lo₂ ∧ hi₁ = hi₂
+--   let loc_eq := if v1.absloc = AbsLoc.bot ∨ v2.absloc = AbsLoc.bot then False
+--                  else v1.absloc = v2.absloc
+--   let unit_eq := if v1.absunit = AbsUnit.bot ∨ v2.absunit = AbsUnit.bot then False
+--                  else v1.absunit = v2.absunit
--- Soundness/GammaSound.lean
+++ Soundness/GammaSound.lean
@@ -5,7 +5,11 @@ import Soundness.Domain
 
 open Set
 
-lemma add_abs_sound_γ (I J : Itv) :
+lemma γValue_int_iff (env : ℙ → Set Base) (a : AbsValue) (z : ℤ) :
+    Value.int z ∈ γValue env a ↔ z ∈ γItv a.absint := by
+    simp [γValue]
+
+lemma add_itv_sound (I J : Itv) :
   add (γItv I) (γItv J) ⊆ γItv (I ⊕ J) := by
   intro z hz
   unfold add at hz
@@ -24,7 +28,38 @@ lemma add_abs_sound_γ (I J : Itv) :
           rcases hz with ⟨x, hx, y, hy, rfl⟩
           rcases hx with ⟨hx_lo, hx_hi⟩
           rcases hy with ⟨hy_lo, hy_hi⟩
-          simp [γItv, add_abs]
+          simp [γItv, addAbs_itv]
           constructor
           · linarith
           · linarith
+
+lemma add_itv_of_val_sound (a1 a2 : AbsValue) :
+    add (γItv a1.absint) (γItv a2.absint) ⊆ γItv (addAbs a1 a2).absint := by
+    intro z hz
+    -- (addAbs a1 a2).absint = a1.absint ⊕ a2.absint 임을 이용
+    have : z ∈ γItv (a1.absint ⊕ a2.absint) := add_itv_sound a1.absint a2.absint hz
+    simpa [addAbs] using this
+
+lemma add_sound (env : ℙ → Set Base) (a1 a2 : AbsValue) :
+addVal (γValue env a1) (γValue env a2) ⊆ γValue env (addAbs a1 a2) := by
+  intro v hv
+  rcases hv with ⟨z1, z2, rfl, hz1, hz2⟩
+
+  -- 1단계: γValue_int_iff 의 left-to-right 방향 사용
+  have hz1' : z1 ∈ γItv a1.absint :=
+    (γValue_int_iff env a1 z1).1 hz1
+  have hz2' : z2 ∈ γItv a2.absint :=
+    (γValue_int_iff env a2 z2).1 hz2
+
+  -- 2단계: z1+z2가 add(γItv a1.absint, γItv a2.absint)에 포함됨
+  have hSum : (z1 + z2) ∈ add (γItv a1.absint) (γItv a2.absint) := by
+    refine ⟨z1, ?_, z2, ?_, rfl⟩
+    · exact hz1'
+    · exact hz2'
+
+  -- 3단계: itv soundness를 이용해 abstract interval에 포함시킨다.
+  have hInItv : (z1 + z2) ∈ γItv (addAbs a1 a2).absint :=
+    add_itv_of_val_sound a1 a2 hSum
+
+  -- 4단계: γValue_int_iff 의 right-to-left 방향 사용
+  exact (γValue_int_iff env (addAbs a1 a2) (z1 + z2)).2 hInItv
